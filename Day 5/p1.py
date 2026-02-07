ff0c1e82c021526c8f7d171e490f6692464158d0
ThakurAmit47 <asthakur98k@gmail.com>
2026-02-07 17:44:13 +0530

    day 5 added

---
+++ Day 5/p1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the Number: "))
+if a == a[::-1]:
+    print("palimdrone")
+
+else:
+    print("None palidrome")
---
+++ Day 5/p4.py
@@ -0,0 +1,3 @@
+a = int(input("Enter the Number is: "))
+for i in range(1, 11):
+    print(f"{a} X {i} = {a*i}")
